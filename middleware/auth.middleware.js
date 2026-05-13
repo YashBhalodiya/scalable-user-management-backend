589b798572e158afa0065d8c7046a30773d1f89f
YashBhalodiya <yashbhalodiya650@gmail.com>
2026-05-13 23:36:11 +0530

    feat: add authentication middleware for token verification and role authorization

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = process.env.JWT_SECRET || 'secretpassword'
+
+const authMiddleware = {
+    async authenticateToken(req, res, next){
+        const authHeader = req.headers['authorization']
+        const token = authHeader && authHeader.split(" ")[1]
+
+        if (!token) {
+            return res.status(401).json({ error: "Access denied. No token provided." });
+        }
+
+        try {
+            const decoded = await jwt.verify(token, JWT_SECRET)
+            req.user = decoded
+            next();
+        } catch (error) {
+            return res.status(403).json({ error: "Invalid or expired token." });
+        }
+    },
+
+    authorizedRole(roles) {
+        return (req, res, next) => {
+            if(!req.user) {
+                return res.status(401).json({error: "User not authenticated"})
+            }
+            if (!roles.includes(req.user.role)) {
+                return res.status(403).json({ error: "Access denied. Insufficient permissions." });
+            }
+            next()
+        }
+    }
+}
+
+module.exports = authMiddleware
